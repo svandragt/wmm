8dfe62d69732da405a62fd10155130c07524329c
Sander van Dragt <sander@vandragt.com>
2022-02-04 13:09:12 +0000

    indent pushd

--- guest.sh
+++ guest.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
 #TODO enable script via symlink
-#TODO enable script requirements
 
 # shellcheck disable=SC1091
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 pushd "$SCRIPT_DIR" || exit 1
-sudo apt-get -y update
+    sudo apt-get -y update
 
-for s in ./scripts-enabled/*.sh ; do
+    for s in ./scripts-enabled/*.sh ; do
+        echo ''
+        . "$s"
+    done
+
+    # TODO move to post
+    sudo systemctl restart caddy
+
+    sudo apt-get -y install net-tools
+    IPADDR=$(hostname -I | cut -d' ' -f1)
     echo ''
-    . "$s"
-done
-
-# TODO move to post
-sudo systemctl restart caddy
-
-sudo apt-get -y install net-tools
-IPADDR=$(hostname -I | cut -d' ' -f1)
-echo ''
-echo "Done! Add the following line to your /etc/hosts"
-echo "${IPADDR} wp.test "
-
+    echo "Done! Add the following line to your /etc/hosts"
+    echo "${IPADDR} wp.test "
 popd || exit 1
