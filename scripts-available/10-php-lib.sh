c5604359cab266cf8dcbcd8e3c0a96d2c41ab1c6
Sander van Dragt <sander@vandragt.com>
2023-01-10 16:59:42 +0000

    Switching to PHP

---
+++ scripts-available/10-php-lib.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+function install_php {
+  PV=$1
+  echo "Installing PHP ${PV}..."
+  sudo apt-get -y install php${PV}-cli php${PV}-fpm php${PV}-mysql php${PV}-curl php${PV}-gd php${PV}-mbstring php${PV}-common php${PV}-xml php${PV}-xmlrpc php${PV}-cli php${PV}-dev php${PV}-zip php${PV}-intl php${PV}-imagick php${PV}-redis
+  sudo cp /multipass/etc/php/fpm/php.ini /etc/php/${PV}/fpm/php.ini
+  sudo systemctl restart php${PV}-fpm
+}
+
+function switch_php {
+  PV=$1
+  echo "Switching to PHP ${PV}..."
+  sudo update-alternatives --set php /usr/bin/php${PV}
+  sudo update-alternatives --set php-fpm.sock /run/php/php${PV}-fpm.sock
+  # Rotate WMM logs
+  cat /multipass/etc/logrotate.d/wmm.tmpl | sed "s/PHP_VERSION/$PHP_VERSION/g" | sudo tee /etc/logrotate.d/wmm
+
+  # Reload services
+	sudo systemctl reload php$PV-fpm
+	if command -v caddy &> /dev/null
+  then
+  	sudo systemctl reload caddy
+  fi
+
+}
