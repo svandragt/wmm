78b82ee0993cf769e8f1ee63f53d6f1130224e93
Sander van Dragt <sander@vandragt.com>
2023-01-23 16:32:51 +0000

    Use the local ubuntu mirror

--- scripts-available/05-system.sh
+++ scripts-available/05-system.sh
@@ -2,6 +2,7 @@
 # Prevent Ubuntu 22.04 asking the user which service should be restarted
 sudo sed -i "/#\$nrconf{restart} = 'i';/a \$nrconf{restart} = 'a';" /etc/needrestart/needrestart.conf
 
+sudo cp /multipass/etc/apt/sources.list /etc/apt/sources.list
 sudo apt-get -y update
 
 if ! command -v avahi-daemon &> /dev/null
