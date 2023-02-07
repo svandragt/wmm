1c974668adae125c1083dfbb6bce83b6705e7088
Sander van Dragt <sander@vandragt.com>
2023-02-07 13:20:51 +0000

    Caddy service must start reliably.

--- scripts-available/20-caddy.sh
+++ scripts-available/20-caddy.sh
@@ -3,3 +3,6 @@ require_caddy
 
 # Update any changes to the template, swapping placeholders
 cat /multipass/etc/caddy/Caddyfile.tmpl | sed "s/PHP_VERSION/$PHP_VERSION/g" | sed "s/WMM_DOMAIN/$WMM_DOMAIN/g"  | sudo tee /etc/caddy/Caddyfile
+# Caddy has to restart often enough until the log mount is ready.
+sudo cp /multipass/lib/systemd/system/caddy.service /lib/systemd/system/caddy.service
+sudo systemctl restart caddy
--- scripts-available/90-post-install.sh
+++ scripts-available/90-post-install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-require_caddy
-sudo systemctl restart caddy
-
 echo ''
 echo "    Site: https://$WMM_DOMAIN"
 echo "    User: admin"
